48d62347c8aa8495a390a86de1b07fb1d5398c58
Sanjana <sanjanaareddy0007@gmail.com>
2026-06-26 12:24:33 +0530

    participant ranking system

---
+++ Day13/participantRankingSystem.cpp
@@ -0,0 +1,31 @@
+#include<bits.stdc++.h>
+using namespace std;
+
+
+vector<string>reorderParticipants(vector<string>&participants, vector<int>& scores) {
+    int n = participants.size();
+    
+    vector<pair<int,string>>arr;
+    for(int i = 0; i < n; i++) {
+        arr.push_back({participants[i] , scores[i]});
+    }
+
+    stable_sort(arr.begin() , arr.end() , [](pair<int,string>a, pair<int ,string>b) {
+        return a.first > b.first;
+    })
+
+    vector<istring>ans;
+    for(auto x : arr) {
+        ans.push_back(x.second);
+    }
+
+    return ans;
+
+}
+
+
+//TC :- O(nlogn)
+//SC :- O(1)
+
+
+
