b3786328bc4a1f1ce0a321a85d89c8bd26216445
Sanjana <sanjanaareddy0007@gmail.com>
2026-06-28 19:44:49 +0530

    practice16

---
+++ Day15/Practice16.cpp
@@ -0,0 +1,22 @@
+#include<bits./stdc++.h>
+using namespace std;
+
+
+vector<int>SortDeliveryPoints(vector<int>&points,int n,int hub) {
+    sort(points.begin(), points.end(),[hub] (int a, int b) {
+        int da = abs(hub  a);
+        int db = abs(hub - b);
+
+        if (da != db) return da < db;
+        return a < b;
+    })
+  
+    return points;
+}
+
+
+
+
+
+
+
