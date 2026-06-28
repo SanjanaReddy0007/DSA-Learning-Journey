64e8c3b924ecfcfb899bffbed8c7d4fec51d46ee
Sanjana <sanjanaareddy0007@gmail.com>
2026-06-28 15:56:04 +0530

    Practice4

---
+++ Day15/Practice4.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int minCost(int n,int c, vector<int>& a) {
+    sort(a.begin(),a.end());
+    int saved = 0;
+
+    for(int x:a) {
+        int currentWeight = 1LL*(1LL<<saved);
+        if(currentWeight <= c) {
+            saved++;
+        }
+    }
+
+    return n - saved;
+
+
+}
+
+
+
+
+
+
+
