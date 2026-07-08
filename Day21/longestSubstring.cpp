0915efdfe873ca79e549f7c60a2754ea275b5a6f
Sanjana <sanjanaareddy0007@gmail.com>
2026-07-08 22:55:10 +0530

    Longest Substringgg

---
+++ Day21/longestSubstring.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+string longestCommonPrefix(vector<string>&words) {
+    int n = words.size();
+    
+    int k = 0;
+    if(n == 0) return "";
+    if(n == 1) return words[0];
+
+    while(true) {
+    for(int i = 1; i < n; i++) {
+        if(k == words[0].size() && k == words[i].size()) {
+            return words[0].substr(0,k);
+        }
+        if(words[i][k] != words[0][k]) {
+            return words[0].substr(0,k);
+        }
+    }
+
+    k++;
+  }
+}
+
+
