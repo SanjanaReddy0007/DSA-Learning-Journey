4ca93e0e59c0f7247a04ff33e565ba453dcfe0f7
Sanjana <sanjanaareddy0007@gmail.com>
2026-07-08 21:09:11 +0530

    Isomorphic

---
+++ Day21/practice.cpp
@@ -0,0 +1,34 @@
+#include<bits.stdc++.h>
+using namespace std;
+
+
+
+bool helper(string s1,string s2) {
+    map<char,char>m;
+
+   if(s1.size() != s2.size()) {
+    return false;
+   }
+   
+   int n = s1.size();
+
+   for(int i = 0; i < n; i++) {
+    if(m.find(s1[i] == m.end())) {
+        m[s1[i]] = s2[i];
+    }
+    else if(s1[i] != s2[i]) {
+        return false;
+    }
+   }
+
+   return true;
+
+}
+
+
+
+bool isomorphic(string s1,string s2) {
+    return helper(s1,s2) && helper(s2,s1);
+}
+
+
