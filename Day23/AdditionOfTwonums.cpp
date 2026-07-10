c0c6cc4ebb94e6bc0e884bac6d6b13fad49004f0
Sanjana <sanjanaareddy0007@gmail.com>
2026-07-10 15:16:55 +0530

    Addition of two nums

---
+++ Day23/AdditionOfTwonums.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    int a,b;
+    cin>>a,b;
+    cout<<a+b<<" ";
+    return 0;
+}
+
+
+//TC:- O(1) print statement performs at constatnt amount of time...
+//SC :_ O(1) only few variable used required constant space.....
+
+
