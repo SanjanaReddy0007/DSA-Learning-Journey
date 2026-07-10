219cd671b79691ea18fa6994cfa278d7c1e67040
Sanjana <sanjanaareddy0007@gmail.com>
2026-07-11 00:14:33 +0530

    ProfitCalculation

---
+++ Day24/CalculateProfit.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+long long calcualteProfit(long long A, long long B, long long C) {
+     return A - (B + c);
+}
+
+//A:- selling sandwitches ......
+//B- amount for bun .....
+//C :- amount for a Vegetabless......
+// SP = cost of selling  and C + B = cost of makinggggggggggggggg
+
+//O(1) only single substratcion and additonal i performed
+
