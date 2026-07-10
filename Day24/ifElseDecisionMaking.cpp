d9fb4a018579fa505fad0261f51934bf500fa1c0
Sanjana <sanjanaareddy0007@gmail.com>
2026-07-11 00:05:31 +0530

    IfElseDecisionMaking

---
+++ Day24/ifElseDecisionMaking.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespacestd;
+
+
+string compariison(int n,int m) {
+    if(n < m) {
+        cout<<"Lesser";
+    }else if (n == m) {
+        cout<<"Equal";
+    } else {
+        cout<<"Greater";
+    }
+}
+
+//TC and SC is O(1) bcs it performs constant operation and space is not addiitonl required/......
+
+
+
