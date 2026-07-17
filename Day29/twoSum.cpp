bda29e17b46745dcedb529ec427d95bef931f915
Sanjana <sanjanaareddy0007@gmail.com>
2026-07-17 13:23:45 +0530

    TWO SUM-USING UNORDERED MAP

---
+++ Day29/twoSum.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int>TwoSum(vector<int>&arr , int k)
+{
+    int n = arr.size();
+    unordered_map<int,int>mp;
+    
+    for(int i = 0; i < n;; i++) {
+        int x = arr[i] - k;
+        if(m.find(x) != m.end()) {
+            return {m[x] , i};
+        } else {
+            return m[arr[i]] = i;
+        }
+    }
+
+    return {-1,-1};
+}
+
+//TC :- O(nlogn) 
+//SC :- O(1)
