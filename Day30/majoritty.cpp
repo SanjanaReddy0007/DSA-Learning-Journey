37bda4ff2697d89a4cf238e386a1c1446ae2d05b
Sanjana <sanjanaareddy0007@gmail.com>
2026-07-22 10:16:02 +0530

    MAjorityyy

---
+++ Day30/majoritty.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//strianght forward approach
+
+int majorityElement(vector<int>&arr)
+{
+    int n = arr.size();
+    sort(arr.begin(),arr.end());
+    return n/2;
+}
+
+//tc:-O(nlogn)
+ //bcs makoirty elm ill be more than half of so it will cross the mid so n/2
+
+
