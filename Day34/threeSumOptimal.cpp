2c79581bc41150856c9644814de485b8f52ce05a
Sanjana <sanjanaareddy0007@gmail.com>
2026-07-26 19:25:40 +0530

    Sum of three equals to zero

---
+++ Day34/threeSumOptimal.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+vector<vector<int>> ThreeSumOptimal(vector<int>&arr)
+{
+    int n = arr.size();
+    vector<int>ans;
+    
+    for(int i = 0; i < n; i++) {
+        if(i >= 1 && arr[i] == arr[i - 1]) contine;
+        int j = i + 1;
+        int k = n - 1;
+
+        while(j < k) {
+            if(j != i + 1 && arr[j] == arr[j + 1]) {
+                j++;
+                contine;
+            }
+            
+            sum += arr[j] + arr[k];
+            if(sum == -arr[i]) {
+                 vector<int>temp = {arr[i],arr[j],arr[k]};
+                 ans.push_back(temp);
+                 j++;
+                 k--;
+            } else if(sum < -arr[i]) {
+                j++;
+            } else{
+                k--;
+            }
+        }
+
+        return ans;
+
+    }
+
+}
+
+//O(n^2)
+//SC :- O(#trplets) + O(n)
+
