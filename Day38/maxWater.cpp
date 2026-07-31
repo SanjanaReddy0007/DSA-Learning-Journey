8a5a9681015dae07b956dfa189a15a37186a4542
Sanjana <sanjanaareddy0007@gmail.com>
2026-07-31 19:22:26 +0530

    container with most water

---
+++ Day38/maxWater.cpp
@@ -0,0 +1,37 @@
+
+
+int maxWater(vector<int>& levels) {
+   int n = levels.size();
+   int maxwater = 0;
+
+   for(int i = 0; i < n; i++) {
+    for(int j = i + 1; j < n; j++) {
+        int water = (j - i) * min(level[i],level[j]);
+        maxwater = max(water , maxwater);
+    }
+   }
+
+  return maxwater;
+}
+
+//O(n^2) SC :- O(1)
+
+
+//OPTIMALLLLLL
+int maxwater(vector<int>& levels) {
+    int n = levels.size();
+    int i = 0;
+    int j = n - 1;
+    int maxwater = 0;
+
+    while(i < j) {
+        int water = (j - i) * min(levl[i] , level[j]);
+        maxwater = max(maxwater , water);
+    }
+
+    return maxwater;
+}
+
+//TC : -O(n)
+//SC : -O(1)
+
