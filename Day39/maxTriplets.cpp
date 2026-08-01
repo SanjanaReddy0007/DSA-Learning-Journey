28e6b2224723fc90f220485a5715a32f3623d391
Sanjana <sanjanaareddy0007@gmail.com>
2026-08-01 07:59:32 +0530

    PRactice set

---
+++ Day39/maxTriplets.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+//BRUTE i<j<k tta condtition is:- value[i] - value[j] + value[k] that the value should be maximum return 
+
+int maximumTripletsValue(vector<int>& triplets)
+{
+  int n = triplets.size();
+  int ans = INT_MIN;
+
+  for(int i = 0; i < n; i++) {
+    for(int j = i + 1; j < n; j++) {
+        if(triplets[i] >= triplets[j]) {
+            continue;
+        }
+
+        for(int k = j + 1; k < n; k++) {
+            if(triplets[j] < triplets[k]) {
+                int current = triplets[i] - triplets[j] + triplets[k];
+                answer = max(answer,current);
+            }
+        }
+    }
+  }
+
+   return answer;
+}
+
+
+//O(n^3)
+//SC :- O(1)
+
---
+++ Day39/megeIntervalssss.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>>mergeIntervals(vector<vector<int>>& intervals) {
+    sort(intervals.begin(),intervals.end());
+    int n = intervals.size();
+    vector<vector<int>> ans;
+
+    int start = intervals[0][0];
+    int end = intervals[0][1];
+
+    for(int i = 1; i < n; i++) {
+        if(end >= intervals[i][0] ) {
+            end = max(end,intervals[i][1]);
+        } else {
+            ans.push_back({start,end});
+            start = intervals[i][0];
+            end = intervals[i][1];
+        }
+    }
+    return ans;
+}
+
+
+//TC :- O(nlogn)
+//SC :- O(n)
+
