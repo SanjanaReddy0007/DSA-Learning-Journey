d935c793981e28c0f96c3872c520a8e477bf7d40
Sanjana <sanjanaareddy0007@gmail.com>
2026-08-08 10:48:16 +0530

    PeakElement

---
+++ Day45/peakElement.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int peakElement(int arr[], int n)
+{
+    if(n == 1) return 0;
+    if(arr[0] > arr[1]) return 0;
+    if(arr[n - 1] > arr[n - 2]) return n - 1;
+
+    for(int i = 1; i < n - 2; i++) {
+        if(arr[i] > arr[i - 1] && arr[i ] > arr[i + 1]) {
+            return i;
+        }
+    }
+
+    return -1;
+
+}
+
+
+
+//O(n)
+//O(1)
