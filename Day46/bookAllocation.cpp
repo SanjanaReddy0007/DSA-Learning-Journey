77e95b0da4775498bc28b6d263f099f6821b315e
Sanjana <sanjanaareddy0007@gmail.com>
2026-08-09 01:04:04 +0530

    Book Allocations

---
+++ Day46/bookAllocation.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int cancount(vector<int>& arr, int maxPages, int k) 
+{
+    int students = 1;
+    long long pages = 0;
+    for(int i = 0; i < n; i++) {
+        if(pages[i] + arr[i] <= maxPages) {
+            pages += arr[i];
+        }else {
+            students++;
+            pages = arr[i];
+        }
+    }
+
+    return students;
+}
+
+int bookAllocation(vector<int>& arr, int k)
+{
+    int n = arr.szie();
+    if(k > n) return -1;
+    int ans = -1;
+
+    int low = *min_element(arr,arr+n);
+    int high = accumulate(arr.begin() , arr.end());
+
+    while(low <= high) {
+        int mid = (low + high) / 2;
+        int students = canCount(arr,mid,k);
+
+        if(students > k) {
+            low = mid + 1;
+        } else {
+            ans = mid;
+            high = mid - 1;
+        }
+    }
+
+    return ans;
+}
+
+
+//O(n*log (high - low + 1))
+//SC :- O(1)
