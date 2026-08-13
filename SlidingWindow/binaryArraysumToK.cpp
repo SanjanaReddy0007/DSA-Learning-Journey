e6a79371f47f95bcd59dea11dfc6351a09202919
Sanjana <sanjanaareddy0007@gmail.com>
2026-08-13 10:22:26 +0530

    BinaryArraySumToK

---
+++ SlidingWindow/binaryArraysumToK.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int binaryArraySumtoK(vector<int>&arr, int k)
+{
+    if(k < 0) return 0;
+
+    int n = arr.size();
+    int i = 0, j = 0, ans = 0, sum  = 0;
+
+    while(j < n) {
+        sum += arr[j];
+
+        while(sum > k) {
+            sum -= arr[i];
+            i++;
+        }
+
+        ans += j - i + 1;
+        j++;
+    }
+
+    return ans;
+
+}
+
+
+int sumtoK(vector<int>&arr, int n)
+{
+    return binaryArraySumtoK(arr,k) - binaryArraySumToK(arr, n - k);
+}
+
+
+//O(n)
+//SC :- O(1)
