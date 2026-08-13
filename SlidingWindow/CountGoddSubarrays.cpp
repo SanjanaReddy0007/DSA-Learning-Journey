eaf821a756d020bc26bf5b25c83397d0ce9118f0
Sanjana <sanjanaareddy0007@gmail.com>
2026-08-13 11:18:12 +0530

    Count Good Subarray

---
+++ SlidingWindow/CountGoddSubarrays.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int countGoodSubarrays(vector<int>&arr, int k)
+{
+    int n = arr.size();
+    int ans = 0;
+    int oddCounts = 0;
+    int validCounts = 0;
+    int left = 0;
+
+    for(int right = 0; right < n; right++) {
+        if(arr[right] % 2 == 1) {
+            oddCounts++;
+            validCounts = 0;
+        }
+
+        while(oddCounts == k) {
+            validCounts++;
+
+            if(arr[left] % 2 == 1) {
+                oddCounts--;
+            }
+          left++;
+        }
+
+        ans += validCounts;
+    }
+    
+
+    return validCounts;
+
+}
+
+
+//O(n)
+//O(1)
