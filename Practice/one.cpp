ab8bc07f1a90d0580f39339c319db3973cfc9c53
Sanjana <sanjanaareddy0007@gmail.com>
2026-08-25 11:10:45 +0530

    RemvoeDuplicatessS

--- Practice/one.cpp
+++ Practice/one.cpp
@@ -1,6 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int removeDuplicates(vector<int>&arr)
+{
 
+    if(arr.empty()) {
+        return 0;
+    }
 
+    int count = 1, i = 1;
+    for(int j = 1; j < n; j++) {
+        if(arr[j] == arr[j -1]) {
+            count++;
+        } else {
+            count = 1;
+        }
+    
 
+    if(count <= 2) {
+        arr[i] = arr[j];
+        i++;
+    }
+}
+
+    return i;
+
+}
+
+//O(n)
+//O(1)
