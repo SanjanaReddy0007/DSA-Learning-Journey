81b0b37362679d06db099d53504c0fa3ba1cbfbb
Sanjana <sanjanaareddy0007@gmail.com>
2026-08-28 21:56:12 +0530

    Repeative

---
+++ Practice/squareOf.cpp
@@ -0,0 +1,10 @@
+vector<int>SquareOfOrderArray(vector<int>&arr) {
+    int n = arr.size();
+
+    for(int i = 0; i < n; i++) {
+        arr[i] = arr[i] * arr[i];
+    }
+
+    sort(arr.begin() , arr.end());
+    return arr;
+}
