7cc81e3d428d82ef83b6597122c93678266b897f
Sanjana <sanjanaareddy0007@gmail.com>
2026-08-28 21:56:55 +0530

    Return

---
+++ Practice/onReturn.cpp
@@ -0,0 +1,16 @@
+int lateReturnFeetotal(vector<int>&delays) {
+  int n = delays.size();
+  int total = 0;
+
+  for(int day : delays) {
+    if(day == 1) {
+       total += 1;
+    } else if(day <= 5) {
+        total += 2*delay;
+    } else {
+        total += 3*delay;
+    }
+  }
+
+  return total;
+}
