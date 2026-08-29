ba43cb7208472577fcee51398dc84bc521b356cb
Sanjana <sanjanaareddy0007@gmail.com>
2026-08-29 14:41:02 +0530

    Dutch national flag

---
+++ Practice/sortLinkedList.cpp
@@ -0,0 +1,23 @@
+class solution{
+   public:
+     Node* sortList(Node *head) {
+        vector<int>arr;
+
+        Node* temp = head;
+        while(temp != nullptr) {
+            arr.push_back(temp->data);
+            temp = temp->next;
+        }
+
+       sort(arr.begin() , arr.end());
+       temp = head;
+
+       for(int i = 0; i < arr.size(); i++) {
+          temp->data = arr[i];
+          temp = temp->next;
+       }
+
+       return head;
+     }
+}
+
