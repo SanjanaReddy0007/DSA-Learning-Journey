bcb9e59380f552b2f150f3cea55221b850ee2860
Sanjana <sanjanaareddy0007@gmail.com>
2026-08-31 00:01:45 +0530

    matrix rotation match

--- Practice/matrixRotation.cpp
+++ Practice/matrixRotation.cpp
@@ -1,2 +1,29 @@
-z
 
+
+bool findRotation(int n, vector<vector<int>>& start, vector<vector<int>>& goal) {
+        //Write your code here...
+        if(start == goal) {
+            return true;
+        }
+
+        int degree = 3;
+
+      while(degree--) {  
+        for(int i = 0; i < n; i++) {
+            for(int j = i; j < n; j++) {
+                swap(matrix[i][j] , matrix[j][i]);
+            }
+        }
+
+        for(int i = 0; i < n; i++) {
+            reverse(matrix[i].begin() , matrix[i].end());
+        }
+
+        if(start == goal) {
+            return true;
+        }
+
+    };
+
+    return false;
+ };
