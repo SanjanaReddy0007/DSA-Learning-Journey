938fb50cf1acf1416b2322d2522bb218b1ba5288
Sanjana <sanjanaareddy0007@gmail.com>
2026-09-01 00:02:57 +0530

    Advance array fourSum

--- Practice/fourSum.cpp
+++ Practice/fourSum.cpp
@@ -1,1 +1,29 @@
 
+vector<vector<int>>fourSum(vector<int>&arr, int target) {
+   int n = arr.size();
+   set<vector<int>>s;
+
+   for(int i = 0; i < n; i++) {
+    for(int j = i + 1; j < n; j++) {
+        for(int k = j + 1; k < n; k++) {
+            for(int l = k + 1; l < n; l++) {
+                long long sum += arr[i];
+                sum += arr[j];
+                sum+= arr[k];
+                sum += arr[l];
+
+                if(sum == target) {
+                    vector<int>temp = {arr[i] , arr[j] , arr[k], arr[l]};
+                    sort(temp.begin() , temp.end());
+                    s.insert(temp);
+                }
+            }
+        }
+     }
+   }
+  
+   vector<vector<int>>ans(s.begin() , s.end());
+   return ans;
+   
+}
+
