a479e29462c5571d112f8b185061897b512351b6
Sanjana <sanjanaareddy0007@gmail.com>
2026-09-03 20:36:41 +0530

    SumOf

---
+++ Practice/sumOf.cpp
@@ -0,0 +1,24 @@
+vector<pair<int,int>>indexPair;
+
+for(int i = 0; i<n;i++) {
+    indexPair.push_back({arr[i],i});
+}
+
+sort(indexArr.begin() , indexArr.end());
+int i = 0, j = n - 1;
+
+while(j < n) {
+    int sum = indexArr[i].first + indexArr[j].first;
+    if(sum == k) {
+        return {indexArr[i].second, indexArr[j].second};
+    }
+
+    if(sum < k) {
+        i++;
+    } else {
+        j++;
+    }
+
+    return {-1,-1};
+
+}
