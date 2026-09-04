a805e30f83946245480d743aec800c86afa3bb63
Sanjana <sanjanaareddy0007@gmail.com>
2026-09-04 12:18:45 +0530

    CountSetBits

---
+++ bitManipulation/checkIfNumisPower.cpp
@@ -0,0 +1,6 @@
+bool isPower (int n) {
+   if(n <= 0) return false;
+   
+   return (n & (n - 1)) == 0;
+}
+
---
+++ bitManipulation/countSetbits.cpp
@@ -0,0 +1,14 @@
+int countSetBits(int n) {
+   int count = 0;
+
+   while((n & 1) == 1) {
+      count++;
+     n = n >> 1;
+   }
+
+   return count;
+}
+
+//TC :- O(log2n)
+//SC :- O(1)
+
