a6973f235642f9efc0a024f10c9ea994094adb30
Sanjana <sanjanaareddy0007@gmail.com>
2026-09-06 01:28:19 +0530

    Single element

---
+++ bitManipulation/singleElement.cpp
@@ -0,0 +1,17 @@
+int singleElement(vector<int>&arr) {
+    int n = arr.size();
+    for(int i = 0; i < n; i++) {
+        int count = 0;
+        for(int j = 0; j < n; j++) {
+            if(arr[i] == arr[j]) {
+                count++;
+            }
+        }
+
+        if(count == 1) {
+            return arr[i];
+        }
+    }
+
+    return -1;
+}
---
+++ bitManipulation/singleElementOptimal.cpp
@@ -0,0 +1,11 @@
+int singleElement(vector<int>&arr) {
+    int n = arr.size();
+    int xor1 = 0;
+
+    for(int i = 0; i < n; i++) {
+        xor1 = xor1 ^ arr[i];
+    }
+
+    return xor1;
+    
+}
