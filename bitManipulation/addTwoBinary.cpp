37465b235224c5fed5b878c1fab6143e96d2e120
Sanjana <sanjanaareddy0007@gmail.com>
2026-09-07 01:40:53 +0530

    ADD Two Binary

---
+++ bitManipulation/addTwoBinary.cpp
@@ -0,0 +1,29 @@
+int twoBinaryBits(string s1, string s2) {
+   int i = s1.length() - 1, j = s2.length();
+   int carry = 0;
+   string ans = "";
+
+   while(i >= 0 || j >= 0) {
+      int sum = carry;
+
+      if(i >= 0) {
+         sum += s1[i] - '0';
+         i--;
+      }
+
+      if(j >= 0) {
+         sum += s2[j] - '0';
+         j--;
+      }
+
+      ans = char((sum & 1) + '0');
+      carry = sum >> 1;
+   }
+
+   if(carry) {
+     ans = '1' + ans;
+   }
+
+   return ans;
+}
+
