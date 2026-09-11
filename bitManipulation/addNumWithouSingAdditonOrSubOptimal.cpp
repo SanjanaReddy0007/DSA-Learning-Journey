8619d4773ef6496c9f903d756da3d538ed6e06b6
Sanjana <sanjanaareddy0007@gmail.com>
2026-09-11 23:14:40 +0530

    AddTwoNumbersWithoutUsingSigns

---
+++ bitManipulation/addNumWithouSingAdditonOrSubOptimal.cpp
@@ -0,0 +1,13 @@
+int addNumsWithoutUsingAdditionAndSubstraction(int a , int b)
+{
+    while(b != 0) {
+       int result = a ^ b;
+       int carry = (a & b) << 1;
+       a = result;
+       b = carry;
+    }
+
+    return a;
+}
+
+//O(1) SC :- O(1)
