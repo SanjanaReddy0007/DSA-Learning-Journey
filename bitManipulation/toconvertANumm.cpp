d7e4382827716a15a3891616e92f5a552c86f858
Sanjana <sanjanaareddy0007@gmail.com>
2026-09-13 10:24:08 +0530

    MinBitFlips To Convert a number

---
+++ bitManipulation/toconvertANumm.cpp
@@ -0,0 +1,15 @@
+int minBitFlipToconvertANumber(int source, int target) {
+   
+    int n = source ^ target;
+    int count = 0;
+
+    while(n != 0) {
+        count++;
+        n = n & (n - 1);
+    }
+
+    return count;
+
+}
+
+
