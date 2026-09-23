75a7c4416e45b78c4a25696d52bfc148ae623b33
Sanjana <sanjanaareddy0007@gmail.com>
2026-09-23 11:29:57 +0530

    ADD ALL CODE

---
+++ Prac/pascalsTrianlgII.cpp
@@ -0,0 +1,14 @@
+vector<vector<int>>getRow(int k) {
+    
+    vector<int>row(1,1);
+    for(int i = 0; i <= k; i++) {
+        row.push_back(1);
+        for(int j = i - 1; j > 0; j--) {
+            row[j] += row[j - 1];
+        }
+    }
+
+    return row;
+
+}
+
---
+++ Prac/pascalsTrianlge.cpp
@@ -0,0 +1,25 @@
+vector<vector<int>>PascalsTrianlge(int n)
+{
+    if(n == 1) {
+        return {{1}};
+    }
+
+    if(n == 2) return {{1},{1,1}};
+    vector<vector<int>>ans = {{1},{1,1}};
+    vector<int>prev = {1,1};
+
+    for(int i = 3; i <= n; i++) {
+        vector<int>cur(i);
+        cur[i] = 1;
+        cur[i - 1] = 1;
+        for(int j = 1; j <= i; j++) {
+            cur[i] = prev[j] + prev[ j - 1];
+        }
+        ans.push_back(cur);
+        cur = prev;
+    }
+
+    return ans;
+
+}
+
