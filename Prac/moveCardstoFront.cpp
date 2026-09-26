694346eb4731840c8a1fab73d078cccab6a55ef0
Sanjana <sanjanaareddy0007@gmail.com>
2026-09-26 23:37:27 +0530

    move to front

--- Prac/moveCardstoFront.cpp
+++ Prac/moveCardstoFront.cpp
@@ -1,4 +1,27 @@
 
+vector<int>moveCardsToFront(vector<int>&cards, vector<int>&req)
+{
+    int n = req.size();
+    unordered_set<int>moved;
+    vector<int>front;
 
+    for(int i = n - 1; i >= 0; i--) {
+        int window = req[i];
+        if(!moved.count(window)){
+            moved.insert(winow);
+            front.push_back(window);
+        }
+    }
+
+    vector<int>ans = front;
+    for(int i = 0; i < cards.size(); i++) {
+        if(!moved.count(cards[i])) {
+            ans.push_back(cards[i]);
+        }
+    }
+
+    return ans;
+
+}
 
 
